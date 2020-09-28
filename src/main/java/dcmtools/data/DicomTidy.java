d890c4fdf9d7b4b61af8dcbc298b32e9d47798f6
Wei Liu <wliu1976@gmail.com>
2020-09-28 22:32:00 +1000

    update

--- src/main/java/dcmtools/data/DicomTidy.java
+++ src/main/java/dcmtools/data/DicomTidy.java
@@ -16,45 +16,44 @@ import dcmtools.util.DicomFileUtils;
 
 public class DicomTidy {
 
-	public static final String DEFAULT_PATH_PATTERN = String.format("{%08X}/{%08X}/{%08X}/{%08X}/{%08X}.dcm",
-			Tag.SourceApplicationEntityTitle, Tag.PatientID, Tag.StudyInstanceUID, Tag.SeriesInstanceUID,
-			Tag.SOPInstanceUID);
+    public static final String DEFAULT_PATH_PATTERN = String.format("{{%08X}/{%08X}/{%08X}.dcm",
+            Tag.StudyInstanceUID, Tag.SeriesInstanceUID, Tag.SOPInstanceUID);
 
-	private static final Logger logger = LogManager.getLogger(DicomTidy.class);
+    private static final Logger logger = LogManager.getLogger(DicomTidy.class);
 
-	private AttributesFormat _format;
-	private boolean _overwrite = false;
+    private AttributesFormat _format;
+    private boolean _overwrite = false;
 
-	public DicomTidy(String pattern, boolean replaceIfExists) {
-		_format = new AttributesFormat(pattern == null ? DEFAULT_PATH_PATTERN : pattern);
-		_overwrite = replaceIfExists;
-	}
+    public DicomTidy(String pattern, boolean replaceIfExists) {
+        _format = new AttributesFormat(pattern == null ? DEFAULT_PATH_PATTERN : pattern);
+        _overwrite = replaceIfExists;
+    }
 
-	public DicomTidy() {
-		this(null, false);
-	}
+    public DicomTidy() {
+        this(null, false);
+    }
 
-	public void tidy(Path srcDir, Path dstDir) throws Exception {
-		Set<Path> dicomFiles = DicomFileUtils.getDicomFiles(srcDir, false);
-		logger.info("found " + dicomFiles.size() + " dicom files in '" + srcDir + "'");
-		for (Path srcDicomFile : dicomFiles) {
-			Attributes attrs = DicomFileUtils.getDicomAttributes(srcDicomFile);
-			Path dstDicomFile = Paths.get(dstDir.toString(), _format.format(attrs));
-			if (Files.exists(dstDicomFile)) {
-				if (_overwrite) {
-					logger.info("moving '" + srcDicomFile + "' to '" + dstDicomFile + "' (already exists. Replace.)");
-					Files.move(srcDicomFile, dstDicomFile, StandardCopyOption.REPLACE_EXISTING);
-				} else {
-					logger.info("'" + dstDicomFile + "' already exists. Ignored.");
-				}
-			} else {
-				logger.info("moving '" + srcDicomFile + "' to '" + dstDicomFile + "'");
-				Path dstParentDir = dstDicomFile.getParent();
-				if (dstParentDir != null && !Files.exists(dstParentDir)) {
-					Files.createDirectories(dstParentDir);
-				}
-				Files.move(srcDicomFile, dstDicomFile);
-			}
-		}
-	}
+    public void tidy(Path srcDir, Path dstDir) throws Exception {
+        Set<Path> dicomFiles = DicomFileUtils.getDicomFiles(srcDir, false);
+        logger.info("found " + dicomFiles.size() + " dicom files in '" + srcDir + "'");
+        for (Path srcDicomFile : dicomFiles) {
+            Attributes attrs = DicomFileUtils.getDicomAttributes(srcDicomFile);
+            Path dstDicomFile = Paths.get(dstDir.toString(), _format.format(attrs));
+            if (Files.exists(dstDicomFile)) {
+                if (_overwrite) {
+                    logger.info("moving '" + srcDicomFile + "' to '" + dstDicomFile + "' (already exists. Replace.)");
+                    Files.move(srcDicomFile, dstDicomFile, StandardCopyOption.REPLACE_EXISTING);
+                } else {
+                    logger.info("'" + dstDicomFile + "' already exists. Ignored.");
+                }
+            } else {
+                logger.info("moving '" + srcDicomFile + "' to '" + dstDicomFile + "'");
+                Path dstParentDir = dstDicomFile.getParent();
+                if (dstParentDir != null && !Files.exists(dstParentDir)) {
+                    Files.createDirectories(dstParentDir);
+                }
+                Files.move(srcDicomFile, dstDicomFile);
+            }
+        }
+    }
 }
